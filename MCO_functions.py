76b7f58f52c2d3f2895d404ab4fdf95c37e463fe
Von Matthew Rojo <vondgrojo@gmail.com>
2026-03-04 23:05:04 +0800

    Added expense distribution

--- MCO_functions.py
+++ MCO_functions.py
@@ -5,4 +5,28 @@ import math
 import pandas as pd
 
 
-def grouped_bar_plot():
+def compareChart(n, cat, figwidth, figheight, urban_columns, rural_columns, title):
+    # bar profile
+    y = np.arange(n) # label location of categories
+    bar_height = 0.4 # the height of bars
+
+    # figure initiation
+    fig, ax = plt.subplots(figsize=(figwidth,figheight))
+
+    # bar profile
+    rects1 = ax.barh(y - 0.2, urban_columns, height=bar_height, label="Urban", color="#0d8ac0")
+    rects2 = ax.barh(y + 0.2, rural_columns, height=bar_height, label="Rural", color="#30e0a8")
+
+    # add labels, title, and legend
+    ax.set_xlabel("Average Monthly Expenses in Php", family="monospace", fontsize=14, fontweight="bold")
+    ax.set_ylabel("Expenses", family="monospace", fontsize=14, fontweight="bold")
+    ax.invert_yaxis()
+    ax.set_title(title, family="impact", fontsize=20)
+    ax.set_yticks(y, cat)
+    plt.legend()
+
+    #add labels to bars
+    ax.bar_label(rects1, padding=3, fmt="{:.2f}")
+    ax.bar_label(rects2, padding=3, fmt="{:.2f}")
+
+    plt.show()
