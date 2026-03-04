0e459ce197b3757f529afb15228e0b65738358e3
Von Matthew Rojo <vondgrojo@gmail.com>
2026-03-04 21:27:41 +0800

    Added EDA on expenses

--- MCO_functions.py
+++ MCO_functions.py
@@ -1,25 +1,8 @@
 
-import numpy as np
-import csv
+import matplotlib.pyplot as plt
+import numpy as np # for importing datasets
+import math
+import pandas as pd
 
-def import_csv_to_numpy(filename):
-    rows = []
-    with open(filename, newline='', encoding='utf-8') as f:
-        reader = csv.reader(f, delimiter=',')
-        header = next(reader, None) # necessary to read header row in strings
-        rows.append(header)
 
-        for row in reader:
-            clean = []
-            for cell in row:
-                cell = cell.strip()
-                if cell == '':
-                    clean.append(np.nan)
-                else:
-                    try:
-                        clean.append(float(cell))
-                    except ValueError:
-                        clean.append(cell)
-            rows.append(clean)
-
-    return np.array(rows, dtype=object) 
+def grouped_bar_plot():
